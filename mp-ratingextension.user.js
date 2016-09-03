2cce68d3f2a46507298a5a70a1a1cbc5a2b9e11a
Kevgaar <rockschlumpf@googlemail.com>
2016-09-03 11:43:54 +0200

    Forgot to set new version number.

--- mp-ratingextension.user.js
+++ mp-ratingextension.user.js
@@ -21,7 +21,7 @@
 //
 // ==UserScript==
 // @name          MoviePilot Rating-Extension
-// @version       2.5
+// @version       2.6
 // @downloadURL   https://github.com/kevgaar/MoviePilot-Rating-Extension/raw/master/mp-ratingextension.user.js
 // @namespace     http://www.moviepilot.de/movies/*
 // @description   Script, mit dem die Bewertungen von IMDb und anderen Plattformen ermittelt und angezeigt werden sollen
